c11aa767d3550bc30d0c04cb505d2417e43112c7
nsgbharath <reddysbharath45@gmail.com>
2023-12-28 10:03:41 +0530

    spring boot demo with jpa repo started

---
+++ src/main/java/com/marolix/set7/springbootdemo/repository/MenuRepository.java
@@ -0,0 +1,9 @@
+package com.marolix.set7.springbootdemo.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+//public interface MenuRepository extends CrudRepository<T, ID> {
+//
+//}
---
+++ src/test/java/com/marolix/set7/springbootdemo/SpringbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marolix.set7.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
